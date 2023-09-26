d33c8905fa4935541aae7aed0f3d9d61dfdee243
Pooja Goyal <poojagoyal5106@gmail.com>
2023-09-26 17:06:49 +0530

    task 8

---
+++ user authentication features and expenses and user table relations/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sequelize = require('./util/database');
+const User = require('./models/user')
+const Expense = require('./models/expenses')
+const userRoutes = require('./routes/userRoutes');
+const expenseRoutes = require('./routes/expenseRoutes');
+
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use('/', userRoutes);
+app.use('/expense',expenseRoutes);
+User.hasMany(Expense);
+Expense.belongsTo(User); 
+
+
+// Start the server
+const PORT =3000;
+sequelize
+  .sync() // This will create the tables if they don't exist.
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
---
+++ user authentication features and expenses and user table relations/controllers/expenseController.js
@@ -0,0 +1,48 @@
+const Expense = require("../models/expenses")
+
+const addexpense = async (req, res) => {
+    try {
+        console.log(req.body);
+        const amount = req.body.amount;
+        const desc = req.body.desc;
+        const cat = req.body.cat;
+        const expense = await Expense.create({ amount: amount, description: desc, category: cat,userId:req.user.id})
+        res.status(201).json({ data: expense });
+  
+    }
+    catch (err) {
+        console.log(err);
+    }
+  }
+  const deleteexpense = async (req, res) => {
+    try {
+        console.log(req.body.id);
+        await Expense.destroy({
+            where: { id: req.body.id },
+        });
+        console.log("expense deleted");
+        res.sendStatus(204);
+    }
+    catch (err) {
+        console.log(err);
+    }
+  }
+  
+  const getexpenses =  async(req, res) => {
+    try {
+        const allExpense = await Expense.findAll({where:{userId:req.user.id}});
+        console.log(allExpense);
+        res.status(200).json({allExpense : allExpense});
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  
+  module.exports = {
+    addexpense,
+    deleteexpense,
+    getexpenses
+  }
+  
